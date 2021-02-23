3ca57ea66811d7e7d9d4764ba3eb1eeba895791b
boturdan <eric.solo@gmail.com>
2021-02-23 11:25:30 +0100

    added under construction gif

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="https://media1.giphy.com/media/xT0xewY8zwC6bva1pK/giphy.gif" width="100px" height="100px"></img>
         <p>
-          Hello!
+          under construction
         </p>
       </header>
     </div>
